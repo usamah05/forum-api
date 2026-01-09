0f3b68b8ad75ceacdc104c468dba485977551ab6
Usamah Abdul Wadud <152948482+usamah05@users.noreply.github.com>
2026-01-09 13:43:21 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
-  // setupFilesAfterEnv: ['<rootDir>/src/Infrastructures/database/postgres/jest.setup.js'],
   collectCoverageFrom: [
     'src/**/*.js',
     '!src/**/_test/**',
